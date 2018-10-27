e402846c37de1a24b3f3fe140ed69840a75a9dd6
shanil <shanil@perfectomobile.com>
2018-10-27 12:21:42 +0300

    Initial Commit

---
+++ Lifting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lifting
+//
+//  Created by Shani Levinkind on 27/10/2018.
+//  Copyright © 2018 Shani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
